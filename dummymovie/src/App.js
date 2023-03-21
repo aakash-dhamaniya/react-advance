e3fc4e17f56aabf7d19307a87b5dde29f68afa51
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-03-22 02:20:58 +0530

    added setInteval to hit api every  5seccond if there is error

--- dummymovie/src/App.js
+++ dummymovie/src/App.js
@@ -1,19 +1,57 @@
 import React, { useState } from "react";
-
 import MoviesList from "./components/MoviesList";
 import "./App.css";
-
+let retrying = null;
+var arr = [];
 function App() {
   const [movies, setMovies] = useState([]);
   const [isloader, setisLoader] = useState(false);
-  async function fetchMoviesHandler() {
+  const [error, setError] = useState(null);
+  const [isCancel, setisCancel] = useState(true);
+  const fetchMoviesHandler = async () => {
+    setError(null);
     setisLoader(true);
-    const res = await fetch("https://swapi.dev/api/films/");
-    const data = await res.json();
-    setMovies(data.results);
-    setisLoader(movies);
-    console.log("hello");
+    try {
+      const res = await fetch("https://swapi.dev/api/film/");
+      if (!res.ok) {
+        throw new Error("Somthing went wrong ...Retrying");
+      }
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (e) {
+      setError(e.message);
+      if (isCancel) {
+        retrying = setInterval(fetchMoviesHandler, 5000);
+        arr.push(retrying);
+      }
+    }
     setisLoader(false);
+  };
+  function cancleRetryingHanldler() {
+    setisCancel(false);
+    console.log(retrying);
+    arr.map((a) => {
+      console.log(a);
+      clearInterval(a);
+      arr = [];
+    });
+    clearInterval(retrying);
+    setError(null);
+    setisCancel(true);
+  }
+  let content = "no data found";
+  if (isloader) {
+    content = <h1>Loading...</h1>;
+  }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = (
+      <div>
+        <p>{error}</p> <button onClick={cancleRetryingHanldler}>Cancel</button>
+      </div>
+    );
   }
   return (
     <React.Fragment>
@@ -21,8 +59,16 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {isloader && <h1>Loading...</h1>}
-        {!isloader && <MoviesList movies={movies} />}
+        {/* {isloader && <h1>Loading...</h1>}
+        {!isloader && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isloader && movies.length === 0 && <h1>Found no movies</h1>}
+        {!isloader && error && (
+          <div>
+            <p>{error}</p>{" "}
+            <button onClick={cancleRetryingHanldler}>Cancel</button>
+          </div>
+        )} */}
+        {content}
       </section>
     </React.Fragment>
   );
